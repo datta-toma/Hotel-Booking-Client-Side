c00bdf2db3402caec470e5bf55726f46e806e3f8
datta-toma <tomadatta15@gmail.com>
2024-05-11 00:02:11 +0600

    featured home page

--- src/pages/Home/FeaturedRooms/FeaturedCard.jsx
+++ src/pages/Home/FeaturedRooms/FeaturedCard.jsx
@@ -1,24 +1,24 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-// const FeaturedCard = ({room}) => {
-//     const {_id, room_img, descriptions} = room;
-//     return (
-//         <div>
-//            <div className="card w-96 bg-base-100 shadow-xl">
-//                 <figure>
-//                     <img src={room_img} alt="Shoes" /></figure>
-//                 <div className="card-body">
-//                 <p><span className="font-medium text-xl">Descriptions: </span>    {descriptions}<span className="font-medium">Read More</span></p>
+const FeaturedCard = ({room}) => {
+    const {_id, room_img, descriptions} = room;
+    return (
+        <div>
+           <div className="card w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img src={room_img} alt="Shoes" /></figure>
+                <div className="card-body">
+                <p><span className="font-medium text-xl">Descriptions: </span>    {descriptions}<span className="font-medium">Read More</span></p>
                 
-//                     <div className="card-actions justify-center">
-//                         <button className="btn btn-primary">Book Now</button>
+                    <div className="card-actions justify-center">
+                        <button className="btn btn-primary">Book Now</button>
                  
-//                     </div>
-//                 </div>
-//                 </div>
-//         </div>
-//     );
-// };
+                    </div>
+                </div>
+                </div>
+        </div>
+    );
+};
 
-// export default FeaturedCard;
+export default FeaturedCard;
--- src/pages/Home/FeaturedRooms/FeaturedRooms.jsx
+++ src/pages/Home/FeaturedRooms/FeaturedRooms.jsx
@@ -1,25 +1,25 @@
-// import { useState, useEffect } from "react"; 
-// import FeaturedCard from "./FeaturedCard";
+import { useState, useEffect } from "react"; 
+import FeaturedCard from "./FeaturedCard";
 
-// const FeaturedRooms = ({ roomCards }) => {
-//     const [roomsToShow, setRoomsToShow] = useState([]);
+const FeaturedRooms = ({ roomCards }) => {
+    const [roomsToShow, setRoomsToShow] = useState([]);
 
-//     useEffect(() => {
-//         if (roomCards && roomCards.length > 0) {
-//             setRoomsToShow(roomCards.slice(0, 9)); // Change to slice the first 9 rooms
-//         }
-//     }, [roomCards]);
+    useEffect(() => {
+        if (roomCards && roomCards.length > 0) {
+            setRoomsToShow(roomCards.slice(0, 9)); // Change to slice the first 9 rooms
+        }
+    }, [roomCards]);
 
-//     return (
-//         <div>
-//             <h3 className="text-4xl mt-5 text-center font-extrabold">Featured Rooms</h3>
-//             <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-4 lg:m-28">
-//                 {roomsToShow.map((room) => (
-//                     <FeaturedCard key={room._id} room={room} />
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
+    return (
+        <div>
+            <h3 className="text-4xl mt-5 text-center font-extrabold">Featured Rooms</h3>
+            <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-4 lg:m-28">
+                {roomsToShow.map((room) => (
+                    <FeaturedCard key={room._id} room={room} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-// export default FeaturedRooms;
+export default FeaturedRooms;
