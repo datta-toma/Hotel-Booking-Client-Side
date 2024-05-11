0443470cf374e0e5359620ba0e54477fca7467f4
datta-toma <tomadatta15@gmail.com>
2024-05-11 21:49:25 +0600

    rooms page review count

--- src/pages/AllRoomCard/RoomsCard.jsx
+++ src/pages/AllRoomCard/RoomsCard.jsx
@@ -1,19 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
+const RoomsCard = ({ roomCard }) => {
+    const { _id, room_img, descriptions, availability } = roomCard;
+    const [totalReviews, setTotalReviews] = useState(0);
 
-const RoomsCard = ({roomCard}) => {
-    const {_id,  room_img, descriptions, availability } = roomCard;
+    useEffect(() => {
+        fetchTotalReviews();
+    }, []);
+
+    const fetchTotalReviews = async () => {
+        try {
+            const response = await fetch(`http://localhost:5000/Rooms/${_id}/reviews/count`);
+            if (response.ok) {
+                const data = await response.json();
+                setTotalReviews(data.totalReviews);
+            } else {
+                console.error('Failed to fetch total review count');
+            }
+        } catch (error) {
+            console.error('Error fetching total review count:', error);
+        }
+    };
+
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
                     <Link to={`/details/${_id}`}>
-                    <img src={room_img} alt="img" className="rounded-xl" />
+                        <img src={room_img} alt="img" className="rounded-xl" />
                     </Link>
-                    
                 </figure>
                 <div className="card-body ">
-                    <p><span className="font-medium text-xl">Descriptions: </span>    {descriptions}<span className="font-medium">Read More</span></p>
+                    <p><span className="font-medium text-xl">Descriptions: </span>{descriptions}<span className="font-medium">Read More</span></p>
+                    <p>Total Reviews: {totalReviews}</p>
                     <div className="card-actions justify-center">
                         {availability ? (
                             <span className="text-green-500">Available</span>
@@ -28,5 +48,3 @@ const RoomsCard = ({roomCard}) => {
 };
 
 export default RoomsCard;
-
-
